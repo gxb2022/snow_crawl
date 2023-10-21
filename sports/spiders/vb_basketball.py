0571a71677eb591b977a0ef04ce389f9840b2a15
bb <gangxiabei@gail.com>
2023-10-22 01:11:32 +0800

    1

--- sports/spiders/vb_basketball.py
+++ sports/spiders/vb_basketball.py
@@ -125,6 +125,6 @@ if __name__ == '__main__':
     settings = get_project_settings()
     process = CrawlerProcess(settings=settings)
     # 实例化爬虫并添加到进程中
-    process.crawl(VbBasketballSpider, ball_time='live')
+    process.crawl(VbBasketballSpider, ball_time='today')
     # 启动爬虫
     process.start()
